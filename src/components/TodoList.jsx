2993d9c87078b8d151855aecdb435af678ecedc7
Bernice Choy <bernice_choy@tech.gov.sg>
2020-09-23 17:58:05 +0800

    test out deploy.yml

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { Button } from "antd";
 import { deleteTodo, updateTodo, setTodos } from "../context/todo.actions";
 import { TodoContext } from "../context/TodoContextProvider";
